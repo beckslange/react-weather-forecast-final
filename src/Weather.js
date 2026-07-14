1dd1ec7b437ed8284ae398ff7e66777a17d96aea
beckslange <beckslange96@gmail.com>
2026-07-13 21:14:09 -0600

    structured app and added styling

--- src/Weather.js
+++ src/Weather.js
@@ -1,25 +1,57 @@
 import React from "react";
+import "./Weather.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Weather() {
   return (
     <div className="Weather">
       <form>
-        <input type="search" placeholder="Enter a city..." required autoFocus />
-        <input type="submit" value="Search" className="btn btn-primary" />
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control form-control-lg"
+              required
+              autoFocus
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary btn-lg w-100"
+            />
+          </div>
+        </div>
       </form>
-      <h1>City Name</h1>
+
+      <h1 className="city-name">New York</h1>
       <ul className="list-unstyled">Monday 07:00</ul>
-      <img
-        src="https://www.gstatic.com/weather/conditions/v1/svg/mostly_cloudy_day_light.svg"
-        alt="weather-icon"
-      />
-      97°F
-      <ul className="list-unstyled">
-        <li>Clear Sky</li>
-        <li>Humidity</li>
-        <li>Wind</li>
-      </ul>
+
+      <div className="row">
+        <div className="col-6">
+          <div className="d-flex align-items-center">
+            <img
+              src="https://www.gstatic.com/weather/conditions/v1/svg/mostly_cloudy_day_light.svg"
+              alt="weather-description"
+              className="weather-icon me-2"
+            />
+            <span className="current-temp">97</span>
+            <span className="unit ms-1">°F</span>
+          </div>
+        </div>
+
+        <div className="col-6 mt-2">
+          <div className="conditions">
+            <ul className="list-unstyled">
+              <li>Clear Sky</li>
+              <li>Humidity</li>
+              <li>Wind</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
