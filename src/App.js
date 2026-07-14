c5da71b73653b4f77f785f5cecccbc3def27e45d
beckslange <beckslange96@gmail.com>
2026-07-13 21:18:59 -0600

    update App.js with footer

--- src/App.js
+++ src/App.js
@@ -2,5 +2,38 @@ import Weather from "./Weather";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App() {
-  return <Weather />;
+  return (
+    <div className="App">
+      <div className="container-fluid px-2">
+        <Weather defaultcity="New York" />
+
+        <footer className="text-center">
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/rebecca-lange-804b8b111/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Rebecca Lange
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/beckslange/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://weather-react-forecast-wk6.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
